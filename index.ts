fcd5b0405eb08e143e5a67a58d09ed643ed4f3bf
LONEWOLF143 <twaha2030@gmail.com>
2024-03-12 10:18:53 +0300

    type annotation

---
+++ index.ts
@@ -0,0 +1,19 @@
+// Annotations
+// Strings
+
+//  let myName: string = 'Mohamed'
+//  console.log(myName)
+
+ // Numbers
+
+//  let favNumber: number = 3;
+//  console.log(favNumber)
+
+// let tsEasy: boolean =  true;
+// tsEasy = false
+// console.log(tsEasy)
+
+// tyep inferrence
+
+let tech = ' Typecsript'
+console.log(typeof tech)
