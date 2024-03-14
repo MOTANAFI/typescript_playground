1d0b3da7e9e47992e4db295781fc2fd12e0bcdcc
LONEWOLF143 <twaha2030@gmail.com>
2024-03-14 10:22:22 +0300

    generics and interfaces

--- index.ts
+++ index.ts
@@ -559,4 +559,35 @@ interface Song {
 // const res1 = getRandonmKeyValuePair<number>(numberObjec)
 // console.log(res1)
 
+// Array
 
+function filterArray<T>(array: T[], condition: (item: T) => boolean): T[] {
+  return array.filter((item) => condition(item));
+}
+
+const numberArray = [1, 2, 3, 4, 5, 6, 6, 7, 8];
+
+const evenNumbers = filterArray<number>(numberArray, (num) => num % 2 === 0);
+
+console.log(evenNumbers);
+
+const stringArr = ['apple', 'banana', 'cherry', 'date'];
+const shortWords = filterArray<string>(stringArr, word => word.length < 6)
+console.log(shortWords)
+
+
+interface Fruit {
+  name: string;
+  color: string
+
+}
+
+const fruitArray: Fruit[] = [
+  {name: 'apple', color: 'Red'},
+  {name: 'Banana', color: 'Yellow'},
+  {name: 'Cherry', color: 'Red'}
+]
+
+
+const redFruits = filterArray<Fruit>(fruitArray, fruit => fruit.color === 'Red')
+console.log(redFruits)
