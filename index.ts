e026a1503dbfce75fa3b95ff8d42294179b41c6b
LONEWOLF143 <twaha2030@gmail.com>
2024-03-14 10:41:15 +0300

    reverse generics

--- index.ts
+++ index.ts
@@ -561,33 +561,40 @@ interface Song {
 
 // Array
 
-function filterArray<T>(array: T[], condition: (item: T) => boolean): T[] {
-  return array.filter((item) => condition(item));
+// function filterArray<T>(array: T[], condition: (item: T) => boolean): T[] {
+//   return array.filter((item) => condition(item));
+// }
+
+// const numberArray = [1, 2, 3, 4, 5, 6, 6, 7, 8];
+
+// const evenNumbers = filterArray<number>(numberArray, (num) => num % 2 === 0);
+
+// console.log(evenNumbers);
+
+// const stringArr = ['apple', 'banana', 'cherry', 'date'];
+// const shortWords = filterArray<string>(stringArr, word => word.length < 6)
+// console.log(shortWords)
+
+
+// interface Fruit {
+//   name: string;
+//   color: string
+
+// }
+
+// const fruitArray: Fruit[] = [
+//   {name: 'apple', color: 'Red'},
+//   {name: 'Banana', color: 'Yellow'},
+//   {name: 'Cherry', color: 'Red'}
+// ]
+
+
+// const redFruits = filterArray<Fruit>(fruitArray, fruit => fruit.color === 'Red')
+// console.log(redFruits)
+
+function reversePair<T, U>(value1: T, value2: U): [U,T] {
+  return [value2, value1]
 }
 
-const numberArray = [1, 2, 3, 4, 5, 6, 6, 7, 8];
-
-const evenNumbers = filterArray<number>(numberArray, (num) => num % 2 === 0);
-
-console.log(evenNumbers);
-
-const stringArr = ['apple', 'banana', 'cherry', 'date'];
-const shortWords = filterArray<string>(stringArr, word => word.length < 6)
-console.log(shortWords)
-
-
-interface Fruit {
-  name: string;
-  color: string
-
-}
-
-const fruitArray: Fruit[] = [
-  {name: 'apple', color: 'Red'},
-  {name: 'Banana', color: 'Yellow'},
-  {name: 'Cherry', color: 'Red'}
-]
-
-
-const redFruits = filterArray<Fruit>(fruitArray, fruit => fruit.color === 'Red')
-console.log(redFruits)
+const reversedPair = reversePair('hello', 20)
+console.log(reversedPair)
