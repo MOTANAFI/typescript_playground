c430b370e08a08d1dc7bd7ba4cc4cdd8b8027c3f
LONEWOLF143 <twaha2030@gmail.com>
2024-03-15 14:33:45 +0300

    ts with axios and expressJS

--- index.ts
+++ index.ts
@@ -699,3 +699,33 @@ interface Song {
 // console.log(manager.role)
 
 // console.log(document)
+
+import axios, {AxiosResponse} from "axios";
+
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean
+}
+
+const fetchData = async () => {
+  try {
+    const response: AxiosResponse<Todo> = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+    console.log('Todo', response.data)
+
+  } catch(error: any) {
+    if(axios.isAxiosError(error)) {
+      console.log('Axios Error', error.message)
+
+      if(error.response) {
+        console.log('status', error.response.status)
+        console.log('Data', error.response.data)
+      }
+    } else {
+      console.log('Error', error.message)
+    }
+  }
+}
+
+fetchData()
